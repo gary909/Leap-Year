c96ac7ec4520d77146adf4e7d32be4fd87106da7
gary <gary.white@lodgit.com>
2021-11-01 16:48:46 +0100

    code working

--- leapYear.js
+++ leapYear.js
@@ -1,9 +1,17 @@
 function leapYear(year){
-    if (year % 4 == 0){
-        return "Leap Year";
+    if((year % 4) == 0 && (year % 100) !== 0 || (year % 400) == 0){
+        return "Leap year.";
     } else {
-        return "Not Leap Year";
+        return "Not leap year.";
     }
 }
 
-console.log(leapYear(2000));
+console.log(leapYear(2000)); // return Leap Year
+console.log(leapYear(2004)); // return Leap Year
+console.log(leapYear(2008)); // return Leap Year
+console.log(leapYear(2012)); // return Leap Year
+console.log(leapYear(2013)); // return Not Leap Year
+console.log(leapYear(2014)); // return Not Leap Year
+console.log(leapYear(1948)); // return Leap Year
+
+//year % 4 == 0 && year % 100 == 0 && year % 400 == 0
